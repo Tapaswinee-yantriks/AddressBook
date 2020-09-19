7f46cc457baa18e6151b6d90c4b4a6ec99ef1a7b
hp pc <tapaswinee.sethy@yantriks.com>
2020-09-19 11:13:11 +0530

    Added:Person Address Book

--- src/com/javacode/AddressBookMain.java
+++ src/com/javacode/AddressBookMain.java
@@ -1,8 +1,19 @@
 package com.javacode;
-
+import java.util.*;
 public class AddressBookMain {
-
+    public class Person{
+        private String firstName;
+        private String lastName;
+    }
+    public class Address{
+        private String city;
+        private String state;
+        private String street;
+        private String zip;
+        private String phoneNumber;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
     }
 }
