034ade8515658c99f358675b2597d7aba8368325
hp pc <tapaswinee.sethy@yantriks.com>
2020-09-19 14:23:25 +0530

    Delete A Person

--- src/com/javacode/AddressBookMain.java
+++ src/com/javacode/AddressBookMain.java
@@ -16,21 +16,29 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         LinkedList<String > list = new LinkedList<String>();
+        int n;
         //List list = new ArrayList();
         list.add("Alias");
         list.add("Bean");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name");
+        System.out.println("Enter a number to Add Names");
+        n = sc.nextInt();
         String firstName = sc.nextLine();
+        System.out.println("Enter  Person Names");
+        for (int i=0; i <= n; i++) {
+            firstName = sc.nextLine();
+            list.add(firstName);
+        }
         list.add(firstName);
-        System.out.println("Enter Last Name");
-        String lastName = sc.nextLine();
-        list.add(lastName);
         System.out.println("Edit Names :" );
         String newName1 = sc.nextLine();
         String newName2 = sc.nextLine();
         list.set(2, newName1);
         list.set(3, newName2);
+        System.out.println("Name: " +list);
+        System.out.println("Delete Location:" );
+        int var = sc.nextInt();
+        list.remove(var);
         System.out.println("Name: " +list);
     }
 }
